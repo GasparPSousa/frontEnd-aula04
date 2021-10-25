62db1ba17af53b262ded0d11627ed9c5dc6d3a80
GasparPSousa <gasparufrj@gmail.com>
2021-10-25 13:01:22 -0300

    Nova alteracao na imagem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import imagem from './img/homem-feliz-dinheiro.jpg'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Aula04</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ1q1Eq0n3TH-4Q9mmwwG8sDAH4du5cLXAxgvVAQWkbX8aPFwlhE7uJjuPl9ZGkBiSFeg&usqp=CAU" alt="fotoDoHomemComDinheiro"/> 
+        <h1>Aula04 - From Gaspar</h1>
+        <img src={imagem} alt="homem feliz" /> 
       </header>
     </div>
   );
